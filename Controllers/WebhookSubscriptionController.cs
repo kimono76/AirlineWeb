587de4fb1313847cdb7814778bcd5416d25dc3cc
AlbertoGruning-Arch <kimono76.live@gmail.com>
2021-08-14 20:08:43 -0300

    cap19-dto

--- Controllers/WebhookSubscriptionController.cs
+++ Controllers/WebhookSubscriptionController.cs
@@ -1,4 +1,5 @@
 using AirlineWeb.Data;
+using AirlineWeb.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AirlineWeb.Controllers
@@ -11,6 +12,10 @@ namespace AirlineWeb.Controllers
         public WebhookSubscriptionController(AirlineDbContext context)
         {
             _context = context;
+        }
+
+        public ActionResult<WebhookSubscriptionReadDto> CreateSubscription(WebhookSubscriptionCreateDto webhookSubscription){
+            
         }
     }
 }
