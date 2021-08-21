cd910371aae18f583ae848a67608068187fe443a
AlbertoGruning-Arch <kimono76.live@gmail.com>
2021-08-21 20:20:54 -0300

    cap24-flightDetailModel

--- Data/AirlineDbContext.cs
+++ Data/AirlineDbContext.cs
@@ -8,5 +8,6 @@ namespace AirlineWeb.Data
        public AirlineDbContext(DbContextOptions<AirlineDbContext> opt):base(opt){}
 
        public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; } 
+       public DbSet<FlightDetail> FlightDetails { get; set; } 
     }
 }
